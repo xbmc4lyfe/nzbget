54b1709e21a11f019d49e19359cabbcbd17e5a8a
dnzbk <denis@nzbget.com>
2026-06-26 17:36:54 +0300

    100ms

--- daemon/nntp/NewsServer.cpp
+++ daemon/nntp/NewsServer.cpp
@@ -39,7 +39,7 @@ NewsServer::NewsServer(int id, bool active, const char* name, const char* host, 
 
 void NewsServer::DelayConnect()
 {
-	constexpr std::chrono::milliseconds CONNECTION_DELAY{15};
+	constexpr std::chrono::milliseconds CONNECTION_DELAY{100};
 	using Clock = std::chrono::steady_clock;
 
 	Clock::time_point deadline{Clock::time_point::max()};
